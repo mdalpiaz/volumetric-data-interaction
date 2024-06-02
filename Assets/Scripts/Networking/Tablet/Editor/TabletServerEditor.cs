675124eb51d8024cef009e5deb955e4706b08f0f
mdalpiaz <80455279+mdalpiaz@users.noreply.github.com>
2024-06-02 17:34:07 +0200

    TabletServerEditor: added buttons for the new commands

--- Assets/Scripts/Networking/Tablet/Editor/TabletServerEditor.cs
+++ Assets/Scripts/Networking/Tablet/Editor/TabletServerEditor.cs
@@ -1,4 +1,6 @@
 using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using UnityEditor;
 using UnityEngine;
 
@@ -13,10 +15,16 @@ namespace Networking.Tablet.Editor
         private MethodInfo _tiltMethod;
         private MethodInfo _shakeMethod;
 
-        private MethodInfo onSelectionMethod;
-        private MethodInfo onSlicingMethod;
+        private MethodInfo onSelection;
+        private MethodInfo onSlicing;
         private MethodInfo onSelect;
         private MethodInfo onDeselect;
+        private MethodInfo onSlice;
+        private MethodInfo onRemoveSnapshot;
+        private MethodInfo onToggleAttached;
+        private MethodInfo onHoldBegin;
+        private MethodInfo onHoldEnd;
+        private MethodInfo onSendToScreen;
 
         private void Awake()
         {
@@ -27,32 +35,40 @@ namespace Networking.Tablet.Editor
             _shakeMethod = typeof(TabletServer).GetMethod("HandleShakes", BindingFlags.NonPublic | BindingFlags.Instance);
 
             // new commands
-
+            onSelection = typeof(TabletServer).GetMethod("OnSelectionMode", BindingFlags.NonPublic | BindingFlags.Instance);
+            onSlicing = typeof(TabletServer).GetMethod("OnSlicingMode", BindingFlags.NonPublic | BindingFlags.Instance);
+            onSelect = typeof(TabletServer).GetMethod("OnSelect", BindingFlags.NonPublic | BindingFlags.Instance);
+            onDeselect = typeof(TabletServer).GetMethod("OnDeselect", BindingFlags.NonPublic | BindingFlags.Instance);
+            onSlice = typeof(TabletServer).GetMethod("OnSlice", BindingFlags.NonPublic | BindingFlags.Instance);
+            onRemoveSnapshot = typeof(TabletServer).GetMethod("OnRemoveSnapshot", BindingFlags.NonPublic | BindingFlags.Instance);
+            onToggleAttached = typeof(TabletServer).GetMethod("OnToggleAttached", BindingFlags.NonPublic | BindingFlags.Instance);
+            onHoldBegin = typeof(TabletServer).GetMethod("OnHoldBegin", BindingFlags.NonPublic | BindingFlags.Instance);
+            onHoldEnd = typeof(TabletServer).GetMethod("OnHoldEnd", BindingFlags.NonPublic | BindingFlags.Instance);
+            onSendToScreen = typeof(TabletServer).GetMethod("OnSendToScreen", BindingFlags.NonPublic | BindingFlags.Instance);
         }
 
-        public override void OnInspectorGUI()
+        public override async void OnInspectorGUI()
         {
             serializedObject.Update();
             
             DrawDefaultInspector();
+            
+            var host = (TabletServer)serializedObject.targetObject;
 
             GUILayout.Label("Modes");
             if (GUILayout.Button("None"))
             {
-                var host = (TabletServer)serializedObject.targetObject;
                 _modeMethod.Invoke(host, new object[] { MenuMode.None });
             }
             
             if (GUILayout.Button("Analysis Mode"))
             {
-                var host = (TabletServer)serializedObject.targetObject;
                 _modeMethod.Invoke(host, new object[] { MenuMode.None });
                 _modeMethod.Invoke(host, new object[] { MenuMode.Analysis });
             }
 
             if (GUILayout.Button("Selection Mode"))
             {
-                var host = (TabletServer)serializedObject.targetObject;
                 _modeMethod.Invoke(host, new object[] { MenuMode.None });
                 _modeMethod.Invoke(host, new object[] { MenuMode.Selection });
             }
@@ -60,63 +76,94 @@ namespace Networking.Tablet.Editor
             GUILayout.Label("Tap");
             if (GUILayout.Button("Double Tap"))
             {
-                var host = (TabletServer)serializedObject.targetObject;
                 _tapMethod.Invoke(host, new object[] { TapType.Double, 250, 250 });
             }
 
             if (GUILayout.Button("Hold Start"))
             {
-                var host = (TabletServer)serializedObject.targetObject;
                 _tapMethod.Invoke(host, new object[] { TapType.HoldBegin, 250, 250 });
             }
             
             if (GUILayout.Button("Hold End"))
             {
-                var host = (TabletServer)serializedObject.targetObject;
                 _tapMethod.Invoke(host, new object[] { TapType.HoldEnd, 250, 250 });
             }
 
             GUILayout.Label("Swipe");
             if (GUILayout.Button("Left"))
             {
-                var host = (TabletServer)serializedObject.targetObject;
                 _swipeMethod.Invoke(host, new object[] { false, 0, 150, 180 });
             }
 
             if (GUILayout.Button("Right"))
             {
-                var host = (TabletServer)serializedObject.targetObject;
                 _swipeMethod.Invoke(host, new object[] { false, 500, 150, 0});
             }
             
             if (GUILayout.Button("Up"))
             {
-                var host = (TabletServer)serializedObject.targetObject;
                 _swipeMethod.Invoke(host, new object[] { false, 250, 300, 90});
             }
 
             GUILayout.Label("Tilt");
             if (GUILayout.Button("Left"))
             {
-                var host = (TabletServer)serializedObject.targetObject;
                 _tiltMethod.Invoke(host, new object[] { true });
             }
 
             if (GUILayout.Button("Right"))
             {
-                var host = (TabletServer)serializedObject.targetObject;
                 _tiltMethod.Invoke(host, new object[] { false });
             }
 
             GUILayout.Label("Shake");
             if (GUILayout.Button("2"))
             {
-                var host = (TabletServer)serializedObject.targetObject;
                 _shakeMethod.Invoke(host, new object[] { 2 });
             }
 
             GUILayout.Label("New Commands");
 
+            if (GUILayout.Button("OnSelectionMode"))
+            {
+                onSelection.Invoke(host, null);
+            }
+            if (GUILayout.Button("OnSlicingMode"))
+            {
+                onSlicing.Invoke(host, null);
+            }
+            if (GUILayout.Button("OnSelect"))
+            {
+                onSelect.Invoke(host, null);
+            }
+            if (GUILayout.Button("OnDeselect"))
+            {
+                onDeselect.Invoke(host, null);
+            }
+            if (GUILayout.Button("OnSlice"))
+            {
+                onSlice.Invoke(host, null);
+            }
+            if (GUILayout.Button("OnRemoveSnapshot"))
+            {
+                onRemoveSnapshot.Invoke(host, null);
+            }
+            if (GUILayout.Button("OnToggleAttached"))
+            {
+                onToggleAttached.Invoke(host, null);
+            }
+            if (GUILayout.Button("OnHoldBegin"))
+            {
+                onHoldBegin.Invoke(host, null);
+            }
+            if (GUILayout.Button("OnHoldEnd"))
+            {
+                onHoldEnd.Invoke(host, null);
+            }
+            if (GUILayout.Button("OnSendToScreen"))
+            {
+                await (Task)onSendToScreen.Invoke(host, null);
+            }
 
             serializedObject.ApplyModifiedProperties();
         }
