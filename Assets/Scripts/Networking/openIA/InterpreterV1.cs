efbe447bfe8ecdba6c88b6071b76f330292597a3
mdalpiaz <80455279+mdalpiaz@users.noreply.github.com>
2024-06-03 13:46:16 +0200

    InterpreterV1: renamed members based on JetBrains Rider naming spec

--- Assets/Scripts/Networking/openIA/InterpreterV1.cs
+++ Assets/Scripts/Networking/openIA/InterpreterV1.cs
@@ -9,43 +9,43 @@ namespace Networking.openIA
 {
     public class InterpreterV1 : ICommandInterpreter, ICommandSender
     {
-        private readonly WebSocketClient _ws;
-        private readonly ProtocolNegotiator _negotiator;
-        private IInterpreterState _state;
+        private readonly WebSocketClient ws;
+        private readonly ProtocolNegotiator negotiator;
+        private IInterpreterState state;
 
         public InterpreterV1(WebSocketClient ws)
         {
-            _ws = ws;
-            _negotiator = new ProtocolNegotiator(this);
-            _state = _negotiator;
+            this.ws = ws;
+            negotiator = new ProtocolNegotiator(this);
+            state = negotiator;
         }
 
-        public async Task Start() => await _negotiator.Negotiate();
+        public async Task Start() => await negotiator.Negotiate();
         
         public async Task Interpret(byte[] data)
         {
             switch (data[0])
             {
                 case Categories.ACK.Value:
-                    _state = await _state.ACK();
+                    state = await state.ACK();
                     break;
                 case Categories.NAK.Value:
-                    _state = await _state.NAK();
+                    state = await state.NAK();
                     break;
                 case Categories.ProtocolAdvertisement.Value:
-                    _state = await _state.ProtocolAdvertisement(data);
+                    state = await state.ProtocolAdvertisement(data);
                     break;
                 case Categories.Client.Value:
-                    _state = await _state.Client(data);
+                    state = await state.Client(data);
                     break;
                 case Categories.Datasets.Value:
-                    _state = await _state.Datasets(data);
+                    state = await state.Datasets(data);
                     break;
                 case Categories.Objects.Value:
-                    _state = await _state.Objects(data);
+                    state = await state.Objects(data);
                     break;
                 case Categories.Snapshots.Value:
-                    _state = await _state.Snapshots(data);
+                    state = await state.Snapshots(data);
                     break;
                 default:
                     Debug.LogError($"Unknown Category received: {BitConverter.ToString(data, 0, 1)}");
@@ -53,6 +53,6 @@ namespace Networking.openIA
             }
         }
 
-        public async Task Send(ICommand cmd) => await _ws.SendAsync(cmd.ToByteArray());
+        public async Task Send(ICommand cmd) => await ws.SendAsync(cmd.ToByteArray());
     }
 }
