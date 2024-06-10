ad996fabfb2493e18f2b26a25f3be282eed008f2
mdalpiaz <80455279+mdalpiaz@users.noreply.github.com>
2024-06-10 15:53:04 +0200

    OpenIAWebSocketClient: send normal and up for viewer

--- Assets/Scripts/Networking/openIA/OpenIAWebSocketClient.cs
+++ Assets/Scripts/Networking/openIA/OpenIAWebSocketClient.cs
@@ -168,8 +168,7 @@ namespace Networking.openIA
             var openIANormal = CoordinateConverter.UnityToOpenIADirection(localNormal);
             var openIAUp = CoordinateConverter.UnityToOpenIADirection(localUp);
             await Send(new SetObjectTranslation(ClientID.Value, openIAPosition));
-            //await Send(new SetObjectRotationNormal(ClientID.Value, openIANormal, openIAUp));
-            await Send(new SetObjectRotationQuaternion(ClientID.Value, Quaternion.LookRotation(openIANormal, openIAUp)));
+            await Send(new SetObjectRotationNormal(ClientID.Value, openIANormal, openIAUp));
         }
 
         private async Task Send(ICommand cmd) => await sender.Send(cmd);
