16c730d0bad314956e72b19f88a8e428a43d1458
mdalpiaz <80455279+mdalpiaz@users.noreply.github.com>
2024-06-10 16:30:31 +0200

    CoordinateConverter: U to O and O to U need different conversions!

--- Assets/Scripts/Networking/openIA/CoordinateConverter.cs
+++ Assets/Scripts/Networking/openIA/CoordinateConverter.cs
@@ -8,36 +8,28 @@ namespace Networking.openIA
     {
         public static Vector3 UnityToOpenIAWorld(Model.Model model, Vector3 position)
         {
-            var localPosition = model.transform.InverseTransformPoint(position);
-            return UnityToOpenIALocal(model, localPosition);
-        }
-
-        public static Vector3 UnityToOpenIALocal(Model.Model model, Vector3 localPosition)
-        {
             var modelUnitToLocal = Mathf.Max(model.Size.x, model.Size.y, model.Size.z);
+            var localPosition = model.transform.InverseTransformPoint(position);
             var diffPosition = model.BottomBackRightCorner - localPosition;
             var newPosition = diffPosition * modelUnitToLocal;
-            return SwapCoordinates(newPosition);
+            return UnityToOpenIACoordinates(newPosition);
         }
 
         public static Vector3 OpenIAToUnityWorld(Model.Model model, Vector3 position)
         {
-            var localPosition = OpenIAToUnityLocal(model, position);
+            var modelUnitToLocal = Mathf.Max(model.Size.x, model.Size.y, model.Size.z);
+            var unityCoordinates = OpenIAToUnityCoordinates(position);
+            var converted = unityCoordinates * modelUnitToLocal;
+            var localPosition = converted + model.BottomBackRightCorner;
             return model.transform.TransformPoint(localPosition);
         }
-        
-        public static Vector3 OpenIAToUnityLocal(Model.Model model, Vector3 position)
-        {
-            var modelUnitToLocal = Mathf.Max(model.Size.x, model.Size.y, model.Size.z);
-            var unityCoordinates = SwapCoordinates(position);
-            var converted = unityCoordinates * modelUnitToLocal;
-            return converted + model.BottomBackRightCorner;
-        }
 
-        public static Vector3 UnityToOpenIADirection(Vector3 direction) => SwapCoordinates(direction);
+        public static Vector3 UnityToOpenIADirection(Vector3 direction) => UnityToOpenIACoordinates(direction);
 
-        public static Vector3 OpenIAToUnityDirection(Vector3 direction) => SwapCoordinates(direction);
+        public static Vector3 OpenIAToUnityDirection(Vector3 direction) => OpenIAToUnityCoordinates(direction);
 
-        private static Vector3 SwapCoordinates(Vector3 vec) => new(-vec.x, vec.z, -vec.y);
+        private static Vector3 OpenIAToUnityCoordinates(Vector3 vec) => new(-vec.x, vec.z, -vec.y);
+
+        private static Vector3 UnityToOpenIACoordinates(Vector3 vec) => new(-vec.x, -vec.z, vec.y);
     }
 }
