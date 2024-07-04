632533b5961a5bb2c3cf633045a11550d02dd13a
mdalpiaz <80455279+mdalpiaz@users.noreply.github.com>
2024-07-04 10:02:40 +0200

    SelectableMaterialChanger: changed member naming

--- Assets/Scripts/Selection/SelectableMaterialChanger.cs
+++ Assets/Scripts/Selection/SelectableMaterialChanger.cs
@@ -14,38 +14,38 @@ namespace Selection
         [SerializeField]
         private Material highlightedMaterial;
         
-        private Selectable _selectable;
-        private MeshRenderer _meshRenderer;
-        private Material _defaultMaterial;
+        private Selectable selectable;
+        private MeshRenderer meshRenderer;
+        private Material defaultMaterial;
         
         private void Awake()
         {
-            _selectable = GetComponent<Selectable>();
-            _meshRenderer = GetComponent<MeshRenderer>();
-            _defaultMaterial = _meshRenderer.material;
+            selectable = GetComponent<Selectable>();
+            meshRenderer = GetComponent<MeshRenderer>();
+            defaultMaterial = meshRenderer.material;
         }
 
         private void OnEnable()
         {
-            _selectable.HighlightChanged += OnHighlightChanged;
-            _selectable.SelectChanged += OnSelectChanged;
+            selectable.HighlightChanged += OnHighlightChanged;
+            selectable.SelectChanged += OnSelectChanged;
         }
 
         private void OnDisable()
         {
-            _selectable.HighlightChanged -= OnHighlightChanged;
-            _selectable.SelectChanged -= OnSelectChanged;
+            selectable.HighlightChanged -= OnHighlightChanged;
+            selectable.SelectChanged -= OnSelectChanged;
         }
         
-        private void OnHighlightChanged(bool isHighlighted) => SetMaterial(isHighlighted ? highlightedMaterial : _defaultMaterial);
+        private void OnHighlightChanged(bool isHighlighted) => SetMaterial(isHighlighted ? highlightedMaterial : defaultMaterial);
 
-        private void OnSelectChanged(bool isSelected) => SetMaterial(isSelected ? selectedMaterial : _defaultMaterial);
+        private void OnSelectChanged(bool isSelected) => SetMaterial(isSelected ? selectedMaterial : defaultMaterial);
 
         private void SetMaterial(Material mat)
         {
-            _meshRenderer.material = mat;
-            _meshRenderer.material.mainTexture = _defaultMaterial.mainTexture;
-            _meshRenderer.material.mainTextureScale = _defaultMaterial.mainTextureScale;
+            meshRenderer.material = mat;
+            meshRenderer.material.mainTexture = defaultMaterial.mainTexture;
+            meshRenderer.material.mainTextureScale = defaultMaterial.mainTextureScale;
         }
     }
 }
